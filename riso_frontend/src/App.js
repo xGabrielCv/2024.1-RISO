988665a34d9a9f1f91c31ff2e6aaa4e9324e3d60
Marco Tulio Soares <mtuliosd2@gmail.com>
2024-08-13 17:13:54 -0300

    inicio front(ainda com alguns problemas) e back ainda precisa ser construido

--- riso_frontend/src/App.js
+++ riso_frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 // IMPORTAÇÕES DE PÁGINAS:
 import Autentication from './pages/Autentication';
 import HomePage from './pages/HomePage';
+import RegisterUnit from './pages/CadastroEmpresa';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Autentication />} />
         <Route path='/homePage' element={<HomePage />} />
+        <Route path="/CadastroEmpresa" element={<RegisterUnit />} />
       </Routes>
     </Router>
   );

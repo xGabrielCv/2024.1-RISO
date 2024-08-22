92cc54808a6870bc7ac5a641088ff2828b3f2b14
DanielRogs <danielrocha.dr142@gmail.com>
2024-08-22 00:46:10 -0300

    feat: adicoinando servicos de delete e update

--- riso_frontend/src/services/userServices.js
+++ riso_frontend/src/services/userServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000';
 
-export function createUser(nameData, emailData, passwordData) {
+export function createUser(nameData, emailData, passwordData){
   const body = {
       name: nameData,
       email: emailData,
@@ -23,3 +23,19 @@ export function loginUser (email, password){
   const response = axios.post(`${baseURL}/user/loginUser`, body);
   return response;
 };
+
+export function updateUser (id, nome, email, password){
+  const body = {}
+
+  if (nome) body.nome = nome;
+  if (email) body.email = email;
+  if (password) body.password = password;
+
+  const response = axios.patch(`${baseURL}/user/updateUser/${id}`, body);
+  return response;
+}
+
+export function deleteUser (id){
+  const response = axios.delete(`${baseURL}/user/deleteUser/${id}`);
+  return response;
+}

fd9b6e8dddbf06c4ca4439336fdabd8f9abb8654
DanielRogs <danielrocha.dr142@gmail.com>
2024-08-24 19:53:28 -0300

    feat: adicionando gerador de codigo aleatorio

--- riso_backend/src/routes/unit.route.js
+++ riso_backend/src/routes/unit.route.js
@@ -4,6 +4,7 @@ import unitController from '../controllers/unit.controller.js';
 const unitRouter = express.Router()
 
 unitRouter.get('/findAllUnit', unitController.findAllUnit);
+unitRouter.get('/findByCode/:code', unitController.findByCodeUnit);
 unitRouter.post('/createUnit',unitController.createUnit);
 unitRouter.patch('/updateUnit/:id', unitController.updateUnit);
 unitRouter.delete('/deleteUnit/:id',unitController.deleteUnit);
